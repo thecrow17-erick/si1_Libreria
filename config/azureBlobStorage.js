6b1942ef549d3883a8bfc69e9ede3ed0a172f989
crow <erickaricari@gmail.com>
2023-07-09 14:56:18 -0400

    conectado a blob_storage azure

---
+++ config/azureBlobStorage.js
@@ -0,0 +1,31 @@
+import azure from 'azure-storage';
+import { config } from 'dotenv';
+
+config({ path: '.env' });
+
+// Configuración de la conexión a Azure Blob Storage
+const blobService = azure.createBlobService(
+  process.env.NOMBRE_CUENTA_STORAGE,
+  process.env.KEY_CUENTA_STORAGE,
+  process.env.HOST_CUENTA_STORAGE
+)
+
+const postImageBlobStorage = ( fileName, filePath) => {
+  return new Promise((resolve, reject) => {
+    blobService.createBlockBlobFromLocalFile(
+      'imagenes',
+      fileName,
+      filePath,
+      (error, result) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(result);
+        }
+      }
+    );
+  });
+};
+
+export default blobService;
+export { postImageBlobStorage };
